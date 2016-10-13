9589b467a23bc69fd9b89aa04f3f3a5cd34b8824
Cloud User <ec2-user@ip-172-31-34-118.us-west-2.compute.internal>
2016-10-13 18:44:29 -0400

    tax homework finished

--- tax_homework.sh
+++ tax_homework.sh
@@ -1,15 +1,20 @@
 function nextBracket {
- echo "nextBracket" 
+  fagi=$1
+  topOfBracket=$2
+  taxRate=$3
+  echo "Your marginal tax rate is : $taxRate"
+  (( delta = $topOfBracket - $1 ))
+  echo "$delta more dollars untill the next bracket"
 } 
 
-read -p "what is your federal adjusted gross income?" fagi
+read -p "what is your federal adjusted gross fagi?" fagi
 
-if [ $fagi -le 9275 ]; then echo 10; nextBracket
-elif [ $fagi -le 37650 ] && [ $fagi -gt 9276 ]; then echo 15; nextBracket #15percent $income
-elif [ $fagi -le 91150 ] && [ $fagi -gt 37651 ]; then echo 25; nextBracket #25percent $income
-elif [ $fagi -le 190150 ] && [ $fagi -gt 91151 ]; then echo 28; nextBracket #28percent $income
-elif [ $fagi -le 413350 ] && [ $fagi -gt 190151 ]; then echo 33; nextBracket #33percent $income
-elif [ $fagi -le 415050 ] && [ $fagi -gt 413351 ]; then echo 35; nextBracket #35percent $income
-elif [ $fagi -gt 415051 ]; then echo 39.6 
-else echo "$income is not valid"
+if [ $fagi -le 9275 ]; then  nextBracket $fagi 9275 10
+elif [ $fagi -le 37650 ] && [ $fagi -gt 9276 ]; then nextBracket $fagi 37650 15   
+elif [ $fagi -le 91150 ] && [ $fagi -gt 37651 ]; then nextBracket $fagi 91150 25   
+elif [ $fagi -le 190150 ] && [ $fagi -gt 91151 ]; then nextBracket $fagi 190150 28
+elif [ $fagi -le 413350 ] && [ $fagi -gt 190151 ]; then nextBracket $fagi 413350 33
+elif [ $fagi -le 415050 ] && [ $fagi -gt 413351 ]; then  nextBracket  $fagi 415050 35
+elif [ $fagi -gt 415051 ]; then echo 39.6 ; echo "you cant get any richer"
+else echo "$fagi is not valid"
 fi
