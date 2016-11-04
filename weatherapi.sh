205adba4a6aa5a5675c0442ab9bc2684785542ca
Cloud User <ec2-user@ip-172-31-34-118.us-west-2.compute.internal>
2016-11-04 12:57:52 -0400

    fall is amazing

--- weatherapi.sh
+++ weatherapi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-read -p "How many days of samples: "               days
-read -p "N) New sample or A) Append to previous: " file_op
+  $1 "How many days of samples: "               days 
+  $2 "N) New sample or A) Append to previous: " file_op 
+
+nohup ./weatherapi.sh 1 N >~/tmp/weatherapi.out 2>&1 &
 
 if [ "$file_op" == "N" ]; then rm -f tmp/weatherapi.sh; fi
 read samples <<< $(($days*24))
@@ -15,19 +17,10 @@ for i in $(seq 1 $samples); do
   if [ $epoch -gt 1477570797 ]; then
     read datetime <<<$(date -d @$epoch +'%m/%d/%Y %H:%M')
     echo "$datetime temp=$temp humidity=$humidity"
-    echo $datetime,$temp,$humidity >> /tmp/weather_data.csv
+    echo $datetime,$temp,$humidity >> ~/tmp/weather_data.csv
   else 
     echo "$(date +'%m/%d/%Y %H:%M') bad sample"
   fi
 
   sleep 60
 done
-
-#read epoch temp humidity <<< $(cat weather.json  | jq '.dt,.main.temp,.main.humidity')
-#read windspeed winddirection <<< $(cat /tmp/weather.json | jq '.wind.speed','.wind.deg')
-
-#while [ true ]
-#do
-curl -s "http://api.openweathermap.org/data/2.5/weather?q=Minneapolis,us&units=imperial&APPID=$weatherapi_key"
-  #sleep 60 
-#done
