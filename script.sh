2628b7c3bc7a80e3daf45ceab791f53a0632997c
Cloud User <ec2-user@ip-172-31-34-118.us-west-2.compute.internal>
2016-12-08 18:18:12 -0500

    session 13

---
+++ script.sh
@@ -0,0 +1,22 @@
+a=$1
+b=$2
+
+function myfunc {
+echo param= $1
+}
+
+myfunc "Hello Kennedy"
+
+#read -p "enter variable c: " c
+#read -p "enter variable d: " d
+
+#echo $a $b $c $d
+
+#for i in {1..10}; do echo $i; done
+
+#a=10
+#for i in $(seq 1 $a); do echo $i; done
+
+#array_examples.sh
+
+
